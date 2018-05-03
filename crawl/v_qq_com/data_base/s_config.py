3da368726c29e81f0c64142ca631bfb05721287a
jercheng <1033937312@qq.com>
2018-05-03 20:38:27 +0800

    test commit2

---
+++ crawl/v_qq_com/data_base/s_config.py
@@ -0,0 +1,11 @@
+config = {
+    "ssh_host": '10.10.10.179',
+    "ssh_port": 8404,
+    "ssh_pwd": "VwKyQMEDnXb5n6Kx",
+    "ssh_user_name": "zhangmaohong",
+    "target_host": "127.0.0.1",
+    "target_port": 3306,
+    "sql_user": "inst",
+    "sql_pwd": "inst@123456ZMH",
+    "db":"video_data",
+}
