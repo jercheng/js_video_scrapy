2400c0b236fb4d4fb4443d486b7e5fc4abaf18a6
jercheng <1033937312@qq.com>
2018-05-03 16:46:07 +0800

    test commit

--- crawl/v_qq_com/data_base/t_main2.py
+++ crawl/v_qq_com/data_base/t_main2.py
@@ -3,6 +3,13 @@ from s_config import config
 def main():
     mc = MysqlConnect(config)
 
+    sql = """ select * from tx_jieshaoye"""
+
+    res = mc.exec_query(sql)
+    print(res)
+
+    mc.close()
+
 
 if __name__ == '__main__':
     main()
