ea273e4ab538c788c4d3447bb9d54e6e9ee4757d
jercheng <1033937312@qq.com>
2018-05-03 16:44:36 +0800

    test commit

--- crawl/v_qq_com/data_base/mysql_connect.py
+++ crawl/v_qq_com/data_base/mysql_connect.py
@@ -3,21 +3,35 @@ from sshtunnel import SSHTunnelForwarder
 
 
 class MysqlConnect:
-    def __init__(self):
+    def __init__(self, config):
+        # self.server = SSHTunnelForwarder(
+        #     ssh_address_or_host=('10.10.10.179', 8404),
+        #     ssh_password="VwKyQMEDnXb5n6Kx",
+        #     ssh_username="zhangmaohong",
+        #     remote_bind_address=('127.0.0.1', 3306)
+        # )
+
         self.server = SSHTunnelForwarder(
-            ssh_address_or_host=('10.10.10.179', 8404),
-            ssh_password="VwKyQMEDnXb5n6Kx",
-            ssh_username="zhangmaohong",
-            remote_bind_address=('127.0.0.1', 3306)
+            ssh_address_or_host=(config["ssh_host"], config["ssh_port"]),
+            ssh_password=config["ssh_pwd"],
+            ssh_username=config["ssh_user_name"],
+            remote_bind_address=(config['target_host'], config['target_port'])
         )
 
         self.server.start()
         # print(server.local_bind_port)
+        # self.client = pymysql.connect(host='127.0.0.1',
+        #                               port=self.server.local_bind_port,
+        #                               user='inst',
+        #                               passwd='inst@123456ZMH',
+        #                               db='video_data',
+        #                               charset='utf8')
+
         self.client = pymysql.connect(host='127.0.0.1',
                                       port=self.server.local_bind_port,
-                                      user='inst',
-                                      passwd='inst@123456ZMH',
-                                      db='video_data',
+                                      user=config['sql_user'],
+                                      passwd=config['sql_pwd'],
+                                      db=config['db'],
                                       charset='utf8')
 
         self.cur = self.client.cursor()
--- crawl/v_qq_com/data_base/t_main2.py
+++ crawl/v_qq_com/data_base/t_main2.py
@@ -1,7 +1,7 @@
 from mysql_connect import MysqlConnect
-
+from s_config import config
 def main():
-    mc = MysqlConnect()
+    mc = MysqlConnect(config)
 
 
 if __name__ == '__main__':
